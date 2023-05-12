df6d1ea128f7445617f032257496550af18b8fa2
bhumika <bhumikakaddugol3011@gmail.com>
2023-05-12 11:59:30 +0530

    intail firled

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+public class Welcome 
+{
+public static void main(String [] args)
+{
+System.out.println("welcome to jenkin");
+
+for(int i=0;i<10;i++)
+{
+System.out.println("welcome to jenkin");
+}
+}
+}
